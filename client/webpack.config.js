bc4e82db1f56460da005d969c349c9be78ee2456
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-08 12:10:40 -0700

    added Mini CSS Extract plugin to webpack config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,6 +22,9 @@ module.exports = () => {
         title: "Text Editor",
       }),
 
+      // CSS Mini Extract Plugin
+      new MiniCssExtractPlugin(),
+
       //Webpack Manifest Plugin
       new WebpackPwaManifest({
         fingerprints: false,
@@ -52,16 +55,20 @@ module.exports = () => {
     module: {
       rules: [
         {
-          test: /|.css$/i,
-          exclude: /node_modules/,
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
           use: {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-env"],
-              plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
-              ],
             },
           },
         },
